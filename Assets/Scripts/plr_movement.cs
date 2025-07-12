ee2d51956fb4f2a6fa554e0bf9fb056800bebac4
Avizarox <garmashov.matvey@yandex.ru>
2025-07-12 19:36:02 +0700

    Насрал у базы2

--- Assets/Scripts/plr_movement.cs
+++ Assets/Scripts/plr_movement.cs
@@ -1,16 +1,98 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class plr_movement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    [Header("References")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private Vector2 moveInput;
+    private bool isGrounded;
+    private bool isFacingRight = true;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // ???????? ?????? ?? ????????
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        // ????????????? ?? ??????? ?????
+        jumpAction.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        // ???????????? ?? ??????? ?????
+        jumpAction.performed -= OnJump;
+    }
+
+    private void Update()
+    {
+        // ????????? ???? ????????
+        moveInput = moveAction.ReadValue<Vector2>();
+
+        // ???????? ?????????? ?? ?????
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // ???????? ?????????
+        if (moveInput.x > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (moveInput.x < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // ????????? ???????? ? FixedUpdate ??? ??????
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    // ???????????? ???? ???????? ????? ? ?????????
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
