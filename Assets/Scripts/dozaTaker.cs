5972bdbd3ab917d257b252ad7d619e4756ee5e30
Avizarox <garmashov.matvey@yandex.ru>
2025-07-13 16:16:34 +0700

    vkusno pokakal

--- Assets/Scripts/dozaTaker.cs
+++ Assets/Scripts/dozaTaker.cs
@@ -2,14 +2,68 @@ using UnityEngine;
 
 public class dozaTaker : MonoBehaviour
 {
+    private SpriteRenderer _char;
     public enum colors { red, blue }
-    void Start()
+
+    public static Item lastPickedItem;
+
+    private void Start()
     {
-        
+        _char = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    public class Item
     {
-        
+        public string Name { get; }
+        public colors Color { get; }
+        public Sprite Icon { get; }
+
+        public Item(string name, colors color, Sprite icon)
+        {
+            Name = name;
+            Color = color;
+            Icon = icon;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            Item newItem = GetItemInfo(other.gameObject);
+
+            lastPickedItem = newItem;
+
+            Destroy(other.gameObject);
+
+            if (newItem.Color == colors.red) _char.color = Color.red;
+            else if (newItem.Color == colors.blue) _char.color = Color.blue;
+
+            Debug.Log(newItem.Color);
+        }
+    }
+
+    Item GetItemInfo(GameObject itemObject)
+    {
+
+        string itemName = itemObject.name.Replace("(Clone)", "").Trim();
+        colors color = colors.red;
+        Sprite icon = itemObject.GetComponent<SpriteRenderer>()?.sprite;
+
+        if (itemName.Contains("blue_doza"))
+        {
+            color = colors.blue;
+        }
+        else if (itemName.Contains("red_doza"))
+        {
+            color = colors.red;
+        }
+
+        return new Item(itemName, color, icon);
+    }
+
+    public Item GetLastPickedItem()
+    {
+        return lastPickedItem;
     }
 }
